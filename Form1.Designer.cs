7f3ebc335a5b71c7e9925fcc434261384b5100f6
N1V33R4 <75680359+N1V33R4@users.noreply.github.com>
2021-12-17 17:26:05 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Number_to_Sound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.readButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Location = new System.Drawing.Point(128, 67);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(106, 17);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Enter a number";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(240, 64);
+            this.numberTextBox.Multiline = true;
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(100, 22);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(151, 139);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(83, 32);
+            this.readButton.TabIndex = 2;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.Location = new System.Drawing.Point(257, 139);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(83, 32);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.readButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(489, 256);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.instructionLabel);
+            this.Name = "Form1";
+            this.Text = "Number to Sound ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace Number_to_Sound
+{
+    public partial class Form1 : Form
+    {
+        private int numToRead;
+        private string numStr;
+
+        private List<string> FilesToPlay = new List<string>();
+
+        private AudioFileReader audio;
+        private IWavePlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void HandleSingleDigit(char digit)
+        {
+            switch (digit)
+            {
+                case '1':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\1_Cut.mp3");
+                    break;
+                case '2':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\2_Cut.mp3");
+                    break;
+                case '3':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\3_Cut.mp3");
+                    break;
+                case '4':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\4_Cut.mp3");
+                    break;
+                case '5':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\5_Cut.mp3");
+                    break;
+                case '6':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\6_Cut.mp3");
+                    break;
+                case '7':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\7_Cut.mp3");
+                    break;
+                case '8':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\8_Cut.mp3");
+                    break;
+                case '9':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\9_Cut.mp3");
+                    break;
+                case '0':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\0_Cut.mp3");
+                    break;
+            }
+        }
+
+        private void HandleTens(string dblDigit)
+        {
+            switch (dblDigit)
+            {
+                case "11":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\11_Cut.mp3");
+                    break;
+                case "12":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\12_Cut.mp3");
+                    break;
+                case "13":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\13_Cut.mp3");
+                    break;
+                case "14":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\14_Cut.mp3");
+                    break;
+                case "15":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\15_Cut.mp3");
+                    break;
+                case "16":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\16_Cut.mp3");
+                    break;
+                case "17":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\17_Cut.mp3");
+                    break;
+                case "18":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\18_Cut.mp3");
+                    break;
+                case "19":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\19_Cut.mp3");
+                    break;
+                case "10":
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\10_Cut.mp3");
+                    break;
+            }
+        }
+
+        private void HandleDoubleDigit(char digit)
+        {
+            switch (digit)
+            {
+                case '1':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\10_Cut.mp3");
+                    break;
+                case '2':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\20_Cut.mp3");
+                    break;
+                case '3':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\30_Cut.mp3");
+                    break;
+                case '4':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\40_Cut.mp3");
+                    break;
+                case '5':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\50_Cut.mp3");
+                    break;
+                case '6':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\60_Cut.mp3");
+                    break;
+                case '7':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\70_Cut.mp3");
+                    break;
+                case '8':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\80_Cut.mp3");
+                    break;
+                case '9':
+                    FilesToPlay.Add(@"C:\Users\User\Desktop\90_Cut.mp3");
+                    break;
+            }
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            /*
+            SpeechSynthesizer speaker = new SpeechSynthesizer
+            {
+                Rate = 1,
+                Volume = 100
+            };
+            speaker.Speak(numberTextBox.Text);
+            */
+
+            /*
+            FilesToPlay.Add(@"C:\Users\User\Desktop\1_Cut.mp3");
+            FilesToPlay.Add(@"C:\Users\User\Desktop\hundred_Cut.mp3");
+            FilesToPlay.Add(@"C:\Users\User\Desktop\and_Cut.mp3");
+            FilesToPlay.Add(@"C:\Users\User\Desktop\2_Cut.mp3");
+            */
+
+            int index = 0;
+                          
+            if (int.TryParse(numberTextBox.Text, out numToRead))
+            {
+                numStr = numberTextBox.Text;
+                
+                if (numToRead > 999)
+                {
+                    if (numStr[index] != '0')
+                    {
+                        HandleSingleDigit(numStr[index]);
+                        FilesToPlay.Add(@"C:\Users\User\Desktop\thousand_Cut.mp3");
+                    }
+                    ++index;
+                }
+
+                if (numToRead > 99)
+                {
+                    if (numStr[index] != '0')
+                    {
+                        HandleSingleDigit(numStr[index]);
+                        FilesToPlay.Add(@"C:\Users\User\Desktop\hundred_Cut.mp3");
+
+                        if (numStr[index + 1] != '0' || numStr[index + 2] != '0')
+                            FilesToPlay.Add(@"C:\Users\User\Desktop\and_Cut.mp3");
+                    }
+                    ++index;
+                }
+
+                if (numToRead > 19)
+                {
+                    if (numStr[index] != '0' && numStr[index] > '1') 
+                    {
+                        HandleDoubleDigit(numStr[index]);
+                    }
+                }
+
+                if (numToRead > 9)
+                {
+                    if (numStr[index] != '0' && numStr[index] == '1')
+                    {
+                        HandleTens(numStr.Substring(index, 2));
+                    }
+                }
+                ++index;
+
+                if (numToRead >= 0)
+                {
+                    if (numStr[index] != '0')
+                    {
+                        HandleSingleDigit(numStr[index]);
+                    }
+                    ++index;
+                }
+            }
+            else
+                MessageBox.Show("Please enter a number!");
+
+            // Play .mp3 files 
+            foreach (var file in FilesToPlay)
+            {
+                audio = new AudioFileReader(file);
+                audio.Volume = 1;
+                player = new WaveOut(WaveCallbackInfo.FunctionCallback());
+                player.Init(audio);
+                player.Play();
+                System.Threading.Thread.Sleep(audio.TotalTime);
+
+                player.Stop();
+                player.Dispose();
+                audio.Dispose();
+                player = null;
+                audio = null;
+            }
+
+            // Reset list 
+            FilesToPlay.Clear();
+        }
+    }
+}
